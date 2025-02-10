e3a541bac4e69021b874f5b6a53d53198541218f
WandesonAndrade <wandesonandrade33@gmail.com>
2025-02-10 17:21:12 -0300

    ajuste da respancividade do header

--- src/components/header/header.component.tsx
+++ src/components/header/header.component.tsx
@@ -1,11 +1,20 @@
 import React, { useEffect, useState } from "react";
-import { FaFacebook, FaInstagram, FaYoutube, FaWhatsapp } from "react-icons/fa";
+import {
+  FaFacebook,
+  FaInstagram,
+  FaYoutube,
+  FaWhatsapp,
+  FaBars,
+  FaTimes,
+} from "react-icons/fa";
 import Logo from "../../element/logo.element";
 import ButtonCliente from "../../element/button.cliente.element";
 import { Link, useLocation } from "react-router-dom";
 
 const Header: React.FC = () => {
   const location = useLocation();
+  const [isScrolled, setIsScrolled] = useState(false);
+  const [menuOpen, setMenuOpen] = useState(false);
 
   useEffect(() => {
     if (location.hash) {
@@ -16,8 +25,6 @@ const Header: React.FC = () => {
       }
     }
   }, [location]);
-
-  const [isScrolled, setIsScrolled] = useState(false);
 
   useEffect(() => {
     const handleScroll = () => {
@@ -32,7 +39,7 @@ const Header: React.FC = () => {
     <nav
       className={`fixed top-0 w-full z-50 bg-gradient-to-r from-blue-900 to-blue-600 p-4 shadow-lg transition-all duration-300 ${
         isScrolled ? "py-2" : "py-4"
-      } `}
+      }`}
     >
       <div className="container mx-auto flex justify-between items-center">
         {/* Logo */}
@@ -44,34 +51,8 @@ const Header: React.FC = () => {
           />
         </div>
 
-        {/* Links de navegação */}
-        <ul className="hidden md:flex space-x-6 text-white">
-          <li className="hover:text-yellow-400 cursor-pointer">
-            <Link to="/#home">Início</Link>
-          </li>
-          <li className="hover:text-yellow-400 cursor-pointer">
-            <Link to="/#plans">Planos</Link>
-          </li>
-          <li className="hover:text-yellow-400 cursor-pointer">
-            <Link to="/about#about">A Empresa</Link>
-          </li>
-          {/*
-          <li className="hover:text-yellow-400 cursor-pointer">
-            <a href="">Cobertura</a>
-          </li>
-          <li className="hover:text-yellow-400 cursor-pointer">
-            <a href="">Dúvidas</a>
-          </li>*/}
-          <li className="hover:text-yellow-400 cursor-pointer">
-            <Link to="/contact#contacts">Contatos</Link>
-          </li>
-          {/*<li className="hover:text-yellow-400 cursor-pointer">
-            Trabalhe Conosco
-          </li>*/}
-        </ul>
-
         {/* Ícones e botões */}
-        <div className="flex items-center space-x-4">
+        <div className="hidden md:flex items-center space-x-4">
           <a href="https://www.facebook.com/paraisointernet">
             <FaFacebook className="text-white text-lg cursor-pointer hover:text-yellow-400" />
           </a>
@@ -90,7 +71,55 @@ const Header: React.FC = () => {
           </a>
           <ButtonCliente />
         </div>
+
+        {/* Menu Mobile */}
+        <div className="md:hidden flex items-center">
+          <button
+            onClick={() => setMenuOpen(!menuOpen)}
+            className="text-white text-2xl"
+          >
+            {menuOpen ? <FaTimes /> : <FaBars />}
+          </button>
+        </div>
       </div>
+
+      {/* Menu Responsivo */}
+      {menuOpen && (
+        <div className="md:hidden absolute top-16 left-0 w-full bg-blue-800 shadow-lg p-4 flex flex-col items-center space-y-4">
+          <Link
+            to="/#home"
+            className="text-white hover:text-yellow-400"
+            onClick={() => setMenuOpen(false)}
+          >
+            Início
+          </Link>
+          <Link
+            to="/#plans"
+            className="text-white hover:text-yellow-400"
+            onClick={() => setMenuOpen(false)}
+          >
+            Planos
+          </Link>
+          <Link
+            to="/about#about"
+            className="text-white hover:text-yellow-400"
+            onClick={() => setMenuOpen(false)}
+          >
+            A Empresa
+          </Link>
+          <Link
+            to="/contact#contacts"
+            className="text-white hover:text-yellow-400"
+            onClick={() => setMenuOpen(false)}
+          >
+            Contatos
+          </Link>
+          <button className="bg-green-500 text-white px-3 py-2 rounded-lg hover:bg-green-600 flex items-center">
+            <FaWhatsapp className="mr-2" /> Assine agora!
+          </button>
+          <ButtonCliente />
+        </div>
+      )}
     </nav>
   );
 };
